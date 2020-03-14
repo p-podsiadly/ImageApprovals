6cac7c51666d35285e261c6407867673aeaa5544
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-03-14 11:42:26 +0100

    t Build single header test with libpng

--- tests/SingleHeader.cpp
+++ tests/SingleHeader.cpp
@@ -1,6 +1,7 @@
 #define APPROVALS_DOCTEST
 #include <ApprovalTests.hpp>
 
+#define ImageApprovals_CONFIG_WITH_LIBPNG
 #define ImageApprovals_IMPLEMENT
 #include <ImageApprovals.hpp>
 
@@ -9,6 +10,6 @@ TEST_CASE("SingleHeader")
     const auto extensions = ImageApprovals::ImageCodec::getRegisteredExtensions();
 
     REQUIRE_EQ(extensions.size(), 2);
-    REQUIRE_EQ(extensions[0], ".exr");
-    REQUIRE_EQ(extensions[1], ".png");
+    REQUIRE_EQ(extensions[0], ".png");
+    REQUIRE_EQ(extensions[1], ".exr");
 }
